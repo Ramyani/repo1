f3b913d841bef502463134fb51c33e7fa3bdeb58
Ramyani <ghoshramyani@gmail.com>
2012-07-24 06:20:04 +0530

    this is my 6th commit of this project

--- ramyani/app/controllers/posts_controller.rb
+++ ramyani/app/controllers/posts_controller.rb
@@ -19,5 +19,9 @@ class PostsController < ApplicationController
 		@user =User.find(params[:user_id])
 		@post =Post.find(params[:id])
 	end
+	def destroy
+		@user =User.find(params[:user_id])
+		@post =Post.find(params[:id]).destroy
+	end
 
 end
